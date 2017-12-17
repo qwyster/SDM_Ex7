e31c496eeb7ffb2577058adb57848d3820849018
Nam Nguyen <Nam Nguyen@NamNguyen-Notebook>
2017-12-17 16:38:53 +0100

    naive bitmapindex finished

--- SDM_CodeBase_Exercise_07_2017/src/de/tuda/sdm/dmdb/access/exercise/NaiveBitmapIndex.java
+++ SDM_CodeBase_Exercise_07_2017/src/de/tuda/sdm/dmdb/access/exercise/NaiveBitmapIndex.java
@@ -14,18 +14,25 @@ import de.tuda.sdm.dmdb.storage.types.AbstractSQLValue;
 import de.tuda.sdm.dmdb.storage.types.exercise.SQLInteger;
 
 /**
- * Bitmap index that uses the vanilla/naive bitmap approach (one bitmap for each distinct value)
+ * Bitmap index that uses the vanilla/naive bitmap approach (one bitmap for each
+ * distinct value)
+ * 
  * @author melhindi
  *
- ** @param <T> Type of the key index by the index. While all abstractSQLValues subclasses can be used,
- * the implementation currently only support for SQLInteger type is guaranteed.
+ ** @param <T>
+ *            Type of the key index by the index. While all abstractSQLValues
+ *            subclasses can be used, the implementation currently only support
+ *            for SQLInteger type is guaranteed.
  */
-public class NaiveBitmapIndex<T extends AbstractSQLValue> extends AbstractBitmapIndex<T>{
+public class NaiveBitmapIndex<T extends AbstractSQLValue> extends AbstractBitmapIndex<T> {
 
 	/*
 	 * Constructor of NaiveBitmapIndex
+	 * 
 	 * @param table Table for which the bitmap index will be build
-	 * @param keyColumnNumbner: index of the column within the passed table that should be indexed
+	 * 
+	 * @param keyColumnNumbner: index of the column within the passed table that
+	 * should be indexed
 	 */
 	public NaiveBitmapIndex(AbstractTable table, int keyColumnNumber) {
 		super(table, keyColumnNumber);
@@ -33,39 +40,47 @@ public class NaiveBitmapIndex<T extends AbstractSQLValue> extends AbstractBitmap
 		this.bitmapSize = this.getTable().getRecordCount();
 		this.bulkLoadIndex();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void bulkLoadIndex() {
 		Iterator<AbstractRecord> it = this.getTable().iterator();
-		while(it.hasNext()){
-			bitMaps.put((T) it.next().getValue(keyColumnNumber), new BitSet(bitmapSize));
-			//it = (Iterator<AbstractRecord>) it.next();
+		while (it.hasNext()) {
+			T key = (T) it.next().getValue(keyColumnNumber);
+			bitMaps.put(key, new BitSet(bitmapSize));
 		}
 		it = this.getTable().iterator();
-		int i = 0;
-		while(it.hasNext()){
+		int i = 1; // bitset indexed from 1
+		while (it.hasNext()) {
 			bitMaps.get(it.next().getValue(keyColumnNumber)).set(i);
 			i++;
-			//it = (Iterator<AbstractRecord>) it.next();
 		}
-}
+	}
+	
+//	void printbitset(BitSet b) {
+//		for (int i = 1; i <= b.size(); i++) {
+//			if (b.get(i) == true) {
+//				System.out.print(i + " ");
+//			}
+//		}
+//		System.out.println();
+//	}
 
 	@Override
 	public List<AbstractRecord> rangeLookup(T startKey, T endKey) {
-		//Iterator<T> it = this.getTable().iterator();
-		BitSet bitset = new BitSet(bitmapSize);
-		bitset.set(0, bitmapSize);
+		BitSet bitset = new BitSet(bitmapSize); // no bits are set
+		
 		List<AbstractRecord> result = new ArrayList<AbstractRecord>();
-		for(HashMap.Entry<T,BitSet> entry : bitMaps.entrySet()){
+		for (HashMap.Entry<T, BitSet> entry : bitMaps.entrySet()) {
 			T key = entry.getKey();
-			if(key.compareTo(startKey)>=0 && key.compareTo(endKey)<=0){
-				bitset.and(entry.getValue());
+			if (key.compareTo(startKey) >= 0 && key.compareTo(endKey) <= 0) {
+				bitset.or(entry.getValue());	// OR 
 			}
 		}
-		for(int i= 0; i<bitmapSize; i++){
-			if(bitset.get(i)){
-				AbstractRecord ar = this.getTable().getRecordFromRowId(i);
+
+		for (int i = 1; i <= bitmapSize; i++) {
+			if (bitset.get(i)==true) {
+				AbstractRecord ar = this.getTable().getRecordFromRowId(i-1);
 				result.add(ar);
 			}
 		}
